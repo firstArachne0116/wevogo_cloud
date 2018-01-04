6021e054b03e501e450ede95e69352f38f963c0c
Sokom Heng <sokomheng89@gmail.com>
2018-01-04 21:57:21 +0700

    phonebook sync for fresh new

--- app/Http/Controllers/Api/PhonebookController.php
+++ app/Http/Controllers/Api/PhonebookController.php
@@ -57,6 +57,7 @@ class PhonebookController extends Controller
 
     public function syncAll(Request $request)
     {
+        Log::debug($request->get('wevo_server_id'));
         $wevoServerId = $request->get('wevo_server_id');
         $contacts = $request->get('contacts');
         PbContact::truncate();
